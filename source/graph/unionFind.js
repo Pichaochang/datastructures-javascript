1ccd8bcd6813baf7c5f560032fdf500d441b65ee
zhangbizhao <pichaochang@163.com>
2022-06-21 11:13:08 +0800

    feat：add Disjoint Set datestructures

---
+++ source/graph/unionFind.js
@@ -0,0 +1,44 @@
+class DisjointSets{
+  constructor(nodes){
+    this.fatherMap = new Map()
+    this.rankMap = new Map()
+    this.makeSets(nodes)
+  }
+  
+  makeSets(){
+    for (let index = 0; index < nodes.length; index++) {
+      const node = nodes[index];
+      fatherMap.set(node, node)
+      rankMap.set(node, 1)
+    }
+  }
+  find(node) {
+    const faththNode = this.fatherMap.get(node)
+    if (faththNode != this.find(faththNode)) {
+      faththNode = this.find(faththNode)
+    }
+    this.fatherMap.set(node, faththNode)
+    return faththNode
+  }
+
+  union(firstNode, secondNode) {
+    if (!firstNode || !secondNode) {
+      return
+    }
+    const firstNodeMap = this.fatherMap.get(firstNode)
+    const secontNodeMap = this.fatherMap.get(secondNode)
+
+    if (firstNodeMap != secontNodeMap) {
+      const firstNodeRank = this.rankMap.get(firstNode)
+      const secontNodeRank = this.rankMap.get(secondNode)
+
+      if (firstNodeRank <= secontNodeMap) {
+        this.fatherMap.set(firstNodeRank, secontNodeMap)
+        this.rankMap.set(secontNodeMap, firstNodeRank + secontNodeRank)
+      } else {
+        this.fatherMap.set(secontNodeMap, firstNodeMap)
+        this.rankMap.set(firstNodeRank, firstNodeRank + secontNodeRank)
+      }
+    }
+  }
+}
