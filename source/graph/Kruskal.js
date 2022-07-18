4dedca7a6413af226fe010c236400e76f6d2e009
pichao chang <31371988+Pichaochang@users.noreply.github.com>
2022-07-18 22:43:57 +0800

    feat: add course code

--- source/graph/Kruskal.js
+++ source/graph/Kruskal.js
@@ -1,7 +1,11 @@
-
 // 了解并查集
 import { DisjointSets } from './unionFind.js'
-console.log(DisjointSets)
-function Kruskal() {
+import { Heap } from '../heap.js'
+function Kruskal(graph) {
+  const unionFind = new DisjointSets(graph.nodes.values())
+  const priorityQueue = new Heap()
+  graph.edges.forEach(edge => {
+    priorityQueue.offer(edge);
+  });
 
 }
